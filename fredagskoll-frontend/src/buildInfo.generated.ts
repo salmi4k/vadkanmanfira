f9f80ec4f7b16f5220ce27c478713f281c9a9a3f
Niklas Dahl <niklas.dahl@biometria.se>
2026-03-15 20:30:34 +0100

    Add celebration rule definitions

--- fredagskoll-frontend/src/buildInfo.generated.ts
+++ fredagskoll-frontend/src/buildInfo.generated.ts
@@ -1,6 +1,6 @@
 export const buildInfo = {
-  "version": "0.1.27",
-  "gitSha": "1e97be8",
-  "buildRef": "1e97be8",
-  "builtAt": "2026-03-15T19:19:40.938Z"
+  "version": "0.1.28",
+  "gitSha": "7bd19f4",
+  "buildRef": "7bd19f4",
+  "builtAt": "2026-03-15T19:29:52.708Z"
 } as const;
