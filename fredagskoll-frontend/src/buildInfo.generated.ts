7d8afa3d42e6dbcadc5a1e50355691f94fd20e80
Niklas Dahl <niklas.dahl@biometria.se>
2026-03-15 08:45:13 +0100

    bump frontend version to 0.1.8

--- fredagskoll-frontend/src/buildInfo.generated.ts
+++ fredagskoll-frontend/src/buildInfo.generated.ts
@@ -1,6 +1,6 @@
 export const buildInfo = {
-  "version": "0.1.7",
-  "gitSha": "d1b8122",
-  "buildRef": "d1b8122",
-  "builtAt": "2026-03-15T07:37:02.430Z"
+  "version": "0.1.8",
+  "gitSha": "e288e74",
+  "buildRef": "e288e74",
+  "builtAt": "2026-03-15T07:44:49.736Z"
 } as const;
